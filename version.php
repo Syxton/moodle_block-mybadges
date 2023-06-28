eff737dc109855eea4d915ce7a8a1b964374e6d9
Matt Davidson <davidso1@rose-hulman.edu>
2023-06-28 10:05:17 -0400

    first release

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018072705;               // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700;               // Requires this Moodle version (3.5).
-$plugin->component = 'block_mybadges'; // Full name of the plugin (used for diagnostics).
-$plugin->release   = '1.3 (2018072701)';
+$plugin->version   = 2023062800;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018051700;            // Requires this Moodle version (3.5).
+$plugin->component = 'block_mybadges';      // Full name of the plugin (used for diagnostics).
+$plugin->release   = '4.2 (2023062800)';
 $plugin->maturity  = MATURITY_STABLE;
